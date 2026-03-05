f258f32d65b7ffa5ef20099588cae34b2e1af6b5
deannaraelarson-hub <deannaraelarson@gmail.com>
2026-03-05 14:59:20 +0100

    Update connect.js

--- wallet/connect.js
+++ wallet/connect.js
@@ -1,21 +1,51 @@
+
 import { createAppKit } from "@walletconnect/appkit";
 import { ethers } from "ethers";
 
+let appkit;
 let provider;
 let signer;
 
 export async function connectWallet() {
 
- const modal = createAppKit({
-  projectId: "906bd57a09299f262aab595f3226ec60",
-  chains: [1,137,56,42161,43114]
- });
+ if(!appkit){
 
- const session = await modal.connect();
+  appkit = createAppKit({
+   projectId: "906bd57a09299f262aab595f3226ec60",
+   networks: [
+    {
+     id: 1,
+     name: "Ethereum",
+     rpcUrl: "https://rpc.ankr.com/eth"
+    },
+    {
+     id: 137,
+     name: "Polygon",
+     rpcUrl: "https://rpc.ankr.com/polygon"
+    },
+    {
+     id: 56,
+     name: "BNB",
+     rpcUrl: "https://rpc.ankr.com/bsc"
+    },
+    {
+     id: 42161,
+     name: "Arbitrum",
+     rpcUrl: "https://rpc.ankr.com/arbitrum"
+    },
+    {
+     id: 43114,
+     name: "Avalanche",
+     rpcUrl: "https://rpc.ankr.com/avalanche"
+    }
+   ]
+  });
 
- provider = new ethers.BrowserProvider(
-  window.ethereum
- );
+ }
+
+ const session = await appkit.open();
+
+ provider = new ethers.BrowserProvider(window.ethereum);
 
  signer = await provider.getSigner();
 
