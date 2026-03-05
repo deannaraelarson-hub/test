a0ab91d30f29ae4e89c55bc9549f5d87217dc430
deannaraelarson-hub <deannaraelarson@gmail.com>
2026-03-05 20:15:39 +0100

    Update connect.js

--- wallet/connect.js
+++ wallet/connect.js
@@ -6,53 +6,34 @@ let modal
 let walletProvider
 
 export async function connectWallet() {
+  if (!modal) {
+    modal = createAppKit({
+      projectId: "906bd57a09299f262aab595f3226ec60",
+      networks: [mainnet, bsc, polygon, arbitrum, avalanche],
+      themeMode: "dark",
+      themeVariables: {
+        "--w3m-accent": "#F7931A",
+        "--w3m-border-radius-master": "8px"
+      },
+      metadata: {
+        name: "NexaWorld Relayer",
+        description: "Execute deposits via relayer",
+        url: window.location.origin,
+        icons: []
+      }
+    })
+  }
 
- if (!modal) {
+  if (!walletProvider) {
+    await modal.open()
+    walletProvider = modal.getWalletProvider()
+  }
 
-  modal = createAppKit({
-   projectId: "906bd57a09299f262aab595f3226ec60",
+  if (!walletProvider) throw new Error("Wallet provider not found")
 
-   networks: [
-    mainnet,
-    bsc,
-    polygon,
-    arbitrum,
-    avalanche
-   ],
+  const provider = new ethers.BrowserProvider(walletProvider)
+  const signer = await provider.getSigner()
+  const address = await signer.getAddress()
 
-   metadata: {
-    name: "NexaWorld",
-    description: "Relayer execution",
-    url: window.location.origin,
-    icons: []
-   }
-  })
-
- }
-
- // If wallet already connected reuse provider
- if (!walletProvider) {
-
-  await modal.open()
-
-  walletProvider = modal.getWalletProvider()
-
- }
-
- if (!walletProvider) {
-  throw new Error("Wallet provider not found")
- }
-
- const provider = new ethers.BrowserProvider(walletProvider)
-
- const signer = await provider.getSigner()
-
- const address = await signer.getAddress()
-
- return {
-  provider,
-  signer,
-  address
- }
-
+  return { provider, signer, address }
 }
